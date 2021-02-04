be5f9c6f8884a865d64a25db753c061d88b7f197
svkalidas <svkalidas@gmail.com>
2021-02-04 09:43:50 +0530

    Python loop changes

--- Python/loop.py
+++ Python/loop.py
@@ -8,13 +8,18 @@ print(f"Employees : {employees}")
 
 employees.sort(reverse=True)
 
+print("For loop")
+
 for employee in employees:
     print(f"Employee Name : {employee}")
 
 count = len(employees)
 
 #While loop in python
+# in python the the nubmer 0 is interpreted as false (positive number : True and zero,negative number :False). The python 
+# interpert the number as boolen only if the number is assosiated with any of the boolean operators like : not, or , and etc
+print("While loop")
 
-while (count > 0):
+while (not not count):
     count = count - 1
     print(f"{count} : {employees[count]}")
